08be2dcea3329995621ecbefb4fb3c6c89f1f707
vksanches <32342498+vksanches@users.noreply.github.com>
2021-06-03 20:13:46 -0300

    teste
    teste

---
+++ aritimetica.js
@@ -0,0 +1,17 @@
+// declarando e inicialiazando variaveis
+let a = 5
+let b = 2
+
+//varios exemplos de operacoes
+
+console.log('a=' + a)
+console.log('b=' + b)
+console.log('a+b=' + (a+b))
+console.log('a-b=' + (a-b))
+console.log('a*b=' + (a*b))
+console.log('a/b=' + (a/b))
+console.log('a % b =' +(a%b))
+console.log('a++=' + (a++))
+console.log('--b=' + (--b))
+console.log('a=' +a)
+console.log('b=' +b)
---
+++ relacionais.js
@@ -0,0 +1,18 @@
+let a= 15
+let b= 12
+let c= '15'
+console.log('a=' +a)
+console.log('b='+b)
+
+console.log('a==b:' + (a==b))
+
+console.log ('a==c:' + (a==c))
+
+console.log('a!==b:'+ (a!==b))
+
+console.log('a!==b:'+ (a!==c))
+
+console.log('a<b:'+ (a<b))
+console.log('a>b:'+ (a>b))
+console.log('a <=b:'+(a<=b))
+console.log('a >=b:'+ (a>=b))
