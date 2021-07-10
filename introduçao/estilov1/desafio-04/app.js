2c48ea2e82b0324962b989f902434ded5862c840
vksanches <vitsanches@live.com>
2021-07-09 21:52:10 -0300

    test

--- introduçao/estilov1/desafio-04/app.js
+++ introduçao/estilov1/desafio-04/app.js
@@ -5,8 +5,12 @@ new Vue({
 		perigo: true,
 		classe3: '',
 		classe4: '',
-		cor:'',
-		classe5:']'
+		cor5:'',
+		estilo5: {
+			height: '100px',
+			width: '100px',
+		},
+		width: '0',
 	},
 	methods: {
 		iniciarEfeito() {
@@ -17,7 +21,12 @@ new Vue({
 
 		},
 		iniciarProgresso() {
-
+			let valor= 0
+			const temporizador= setInterval(() =>{
+				valor+= 5
+				this.width = '${valor}%'
+				if(valor == 100) clearInterval(temporizador)
+			}, 500)			
 			},
 
 		setPerigo (event){
