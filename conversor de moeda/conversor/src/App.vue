b649e5d78a544af00104f1d236984e293b13c703
vksanches <vitsanches@live.com>
2021-09-16 14:54:28 -0300

    Exercicio de lista de tarefas pronto

---
+++ conversor de moeda/conversor/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+
+    <Conversor moedaA="USD" moedaB="BRL"/>
+
+  </div>
+</template>
+
+<script>
+import Conversor from './components/Conversor.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Conversor
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
